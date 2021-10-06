1436a87acbacd5325110463fdd2ec57c8f1d00a1
Rizqon <m.rum.rowi.r@gmail.com>
2021-10-06 16:23:39 +0700

    rename .stub -> .php

--- src/Commands/initModuleBackendCommand.php
+++ src/Commands/initModuleBackendCommand.php
@@ -49,7 +49,7 @@ class initModuleBackendCommand extends Command
         mkdir($path);
         // copy module service-provider
         copy(
-            __DIR__ . DIRECTORY_SEPARATOR . 'Stubs' . DIRECTORY_SEPARATOR . 'moduleBackendServiceProvider.stub', 
+            __DIR__ . DIRECTORY_SEPARATOR . 'Stubs' . DIRECTORY_SEPARATOR . 'moduleBackendServiceProvider.php', 
             $path . DIRECTORY_SEPARATOR . 'moduleBackendServiceProvider.php'
         );
 

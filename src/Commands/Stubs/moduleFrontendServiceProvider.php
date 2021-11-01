76a4d40c27fde1ce30eabb6e9730ef084b9acd8e
Kalingrowo <kalingrowo@gmail.com>
2021-11-01 20:55:35 +0700

    fix: updated module-frontend-service-providers

--- src/Commands/Stubs/moduleFrontendServiceProvider.php
+++ src/Commands/Stubs/moduleFrontendServiceProvider.php
@@ -42,14 +42,12 @@ class moduleFrontendServiceProvider extends ServiceProvider
 
         foreach($filesystem as $modules) {
             $moduleName = last(explode(DIRECTORY_SEPARATOR, $modules));
-
             if(is_dir(app_path() . DIRECTORY_SEPARATOR . $path . DIRECTORY_SEPARATOR . $moduleName . DIRECTORY_SEPARATOR . 'Providers')) {
-                $path = str_replace(DIRECTORY_SEPARATOR, '\\', $path);
-                $this->app->register("App\\{$path}\\{$moduleName}\\Providers\\routeServiceProvider");
+                $tempPath = str_replace(DIRECTORY_SEPARATOR, '\\', $path);
+                $this->app->register("App\\{$tempPath}\\{$moduleName}\\Providers\\routeServiceProvider");
             }else{
                 $this->initiateProvider($path . DIRECTORY_SEPARATOR . $moduleName);
             }
         }
     }
-
 }

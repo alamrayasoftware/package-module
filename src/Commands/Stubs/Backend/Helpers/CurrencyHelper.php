9704426c8b978fd3be9ba98a5915bf7b416d799e
Kalingrowo <kalingrowo@gmail.com>
2022-02-01 20:37:00 +0700

    refactor: modified currency-helper to standar function

--- src/Commands/Stubs/Backend/Helpers/CurrencyHelper.php
+++ src/Commands/Stubs/Backend/Helpers/CurrencyHelper.php
@@ -1,17 +1,20 @@
 <?php
 
-if (!function_exists('deformatCurrency')) {
+namespace App\Helpers;
 
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Route;
+
+class CurrencyHelper
+{
     /**
      * Format currency from string to int
      * 
-     * @param string $currency currency data in string ( ex: 12,000.00 )
+     * @param string|int $currency currency data in string ( ex: 12,000.00 )
      * 
      * @return float 
      */
-    function deformatCurrency(
-        string $currency = null
-    ) {
+    public function deformatCurrency($currency = null) {
         if (!$currency) {
             return 0;
         }

3f50a294ac6d65c116483253d0af2cc6bbab762f
Kalingrowo <kalingrowo@gmail.com>
2022-03-16 16:22:26 +0700

    fix: changed module name TransferStocks -> TransferGoods

--- src/Commands/Stubs/Backend/Inventory/TransferGoods/Controllers/TransferGoodsController.php
+++ src/Commands/Stubs/Backend/Inventory/TransferGoods/Controllers/TransferGoodsController.php
@@ -17,7 +17,7 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\DB;
 
-class TransferStocksController extends Controller
+class TransferGoodsController extends Controller
 {
     public $responseFormatter, $loggerHelper;
 
